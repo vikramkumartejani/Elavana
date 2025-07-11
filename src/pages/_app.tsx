497e2538711f1625d19834487822e80f54f847a9
vikram kumar <vikramkumar8t8@gmail.com>
2025-07-12 03:14:46 +0500

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,12 @@
 import type { AppProps } from "next/app";
-import "../styles/globals.css";
 import "../styles/swiper.css";
+import '../style/globals.css'
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+    return <Component {...pageProps} />;
 }
 
 export default MyApp;
